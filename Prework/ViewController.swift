5b759e0b87ab1e5809c4556ee8137d68b112258a
yunsanch <60577975+yunsanch@users.noreply.github.com>
2022-07-20 13:52:59 -0400

    making text field for the bill amount become the first responder

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        billAmountTextField.becomeFirstResponder()
         //sets the tittle in the navegation bar
         self.title = "Tip calculator"
     }
